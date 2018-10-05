e141d4119ae9f5fc18703c5c6af2bc71c25e5355
Junjun Jiang <junjun0595@163.com>
2018-10-05 20:53:27 +0900

    Update TLcR_RL.m

--- utilities/TLcR_RL.m
+++ utilities/TLcR_RL.m
@@ -7,8 +7,7 @@ overlap_FLAG = zeros(imrow,imcol);
 
 U = ceil((imrow-overlap)/(patch_size-overlap));  
 V = ceil((imcol-overlap)/(patch_size-overlap)); 
-% xXF = [];xX = [];
-% load('XXFXX.mat','xXF','xX');
+
 for i = 1:U
     fprintf('.');
      for j = 1:V  
@@ -30,44 +29,30 @@ for i = 1:U
         % obtain the LR and HR training patches
         padpixel = (window-patch_size)/stepsize;
         XF = Reshape3D_20Connection(YH,BlockSize,stepsize,padpixel);
-        X  = Reshape3D_20Connection_Spatial(YL,BlockSizeS,stepsize,padpixel,c);
-        
-%         xXF = [xXF XF];
-%         xX = [xX X];
-% XF = xXF;
-% X  = xX;
-        
+        X  = Reshape3D_20Connection_Spatial(YL,BlockSizeS,stepsize,padpixel,c);        
+       
         % obtain the LR training patch feature by subtracting its mean
         X(1:end-2,:) = X(1:end-2,:)-repmat(mean(X(1:end-2,:)),size(X(1:end-2,:),1),1);
-%         X(1:end,:) = X(1:end,:)-repmat(mean(X(1:end,:)),size(X(1:end,:),1),1);
-
-
-      
+     
         % calculate the distances between current patch and the LR training patches
         nframe =  size(im_l_patch',1);
         nbase  =  size(X',1);
         XX     =  sum(im_l_patch'.*im_l_patch', 2);        
         SX     =  sum(X'.*X', 2);
-        D      =  repmat(XX, 1, nbase)-2*im_l_patch'*X+repmat(SX', nframe, 1);
-        
+        D      =  repmat(XX, 1, nbase)-2*im_l_patch'*X+repmat(SX', nframe, 1);        
 
-%         K = size(D,2)
         % thresholding      
         [val,index]=sort(D);        
         Xk  = X(:,index(1:K));        
         XFk = XF(:,index(1:K));      
         Dk = D(index(1:K));
-        
-%         im_l_patch = im_l_patch(1:end-2);
-%         Xk = Xk(1:end-2,:);
-        
+
         % Compute the optimal weight vector  for the input LR image patch  with the LR training image patches at position（i,j）
         z   =  Xk' - repmat(im_l_patch', K, 1);         
         C   =  z*z';                                                
         C   =  C + tau*diag(Dk)+eye(K,K)*(1e-6)*trace(C);   
         w   =  C\ones(K,1);  
         w   =  w/sum(w);    
-
        
         % obtain the HR patch with the same weight vector w
         Img  =  XFk*w; 
